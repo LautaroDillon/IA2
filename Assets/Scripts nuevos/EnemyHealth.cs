ebca87c6c06cfb5766459d805947622c61427ac5
LautaroDillon <134712364+LautaroDillon@users.noreply.github.com>
2024-09-29 18:53:07 -0300

    arreglos

--- Assets/Scripts nuevos/EnemyHealth.cs
+++ Assets/Scripts nuevos/EnemyHealth.cs
@@ -1,32 +1,13 @@
 using UnityEngine;
 using System.Linq;
 
-public class PlayerShooting : MonoBehaviour
+public class EnemyHealth: MonoBehaviour
 {
-    public GameObject bulletPrefab;
-    public Transform firePoint;
-    public int maxAmmo = 10;
-    private int currentAmmo;
 
-    void Start()
+    public int health;
+
+    public void TakeDamage(int dmg)
     {
-        currentAmmo = maxAmmo;
-    }
 
-    void Update()
-    {
-        if (Input.GetButtonDown("Fire1") && currentAmmo > 0)
-        {
-            Shoot();
-            currentAmmo--;
-        }
-
-        // LINQ: Limitar la cantidad de balas disparadas
-        var ammoRemaining = Enumerable.Range(1, maxAmmo).TakeWhile(a => a <= currentAmmo).ToList();
-    }
-
-    void Shoot()
-    {
-        Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
     }
 }
--- Assets/Scripts nuevos/Player Shooting.cs
+++ Assets/Scripts nuevos/Player Shooting.cs
@@ -1,18 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class PlayerShooting : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject bulletPrefab;
+    public Transform firePoint;
+    public int maxAmmo = 10;
+    private int currentAmmo;
+
     void Start()
     {
-        
+        currentAmmo = maxAmmo;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButtonDown("Fire1") && currentAmmo > 0)
+        {
+            Shoot();
+            currentAmmo--;
+        }
+
+        // LINQ: Limitar la cantidad de balas disparadas
+        var ammoRemaining = Enumerable.Range(1, maxAmmo).TakeWhile(a => a <= currentAmmo).ToList();
+    }
+
+    void Shoot()
+    {
+        Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
     }
 }
